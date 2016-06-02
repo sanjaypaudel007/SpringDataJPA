26a83af5b50ff54c7021f654a87dcf4dbb3fcc90
Sanjay Paudel <sanjaypaudel007@gmail.com>
2016-06-02 12:15:12 -0500

    Add query DSL for findByStateLike

--- src/main/java/com/guitar/db/repository/LocationJpaRepository.java
+++ src/main/java/com/guitar/db/repository/LocationJpaRepository.java
@@ -1,5 +1,7 @@
 package com.guitar.db.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,6 @@ import com.guitar.db.model.Location;
 
 @Repository
 public interface LocationJpaRepository extends JpaRepository<Location, Long> {
+	public List<Location> findByStateLike(String state);
 
 }
